726cdecf6202860d67d5e0b628b10ee74f61334d
ngoing <sandra.emmy@yahoo.com>
2026-04-15 13:21:38 +0200

    Add login function to Feature-login branch

--- login.py
+++ login.py
@@ -2,6 +2,3 @@ def login(username, password):
     if username == "admin" and password == "password123": 
         return True 
     return False 
- 
-def validate_username(username): 
-    return len(username) > 3 
